62d00191103c1de6946d1dfd4d19c4fab1197959
Joakim Soderlund <joakim.soderlund@gmail.com>
2019-04-09 11:48:11 +0200

    Make constants customizable

--- public/publish.php
+++ public/publish.php
@@ -4,17 +4,26 @@ $root = realpath(dirname(__DIR__));
 
 require_once "$root/vendor/autoload.php";
 
-define('WPR_LOG', 'php://stdout');
-define('WPR_CACHE', "$root/cache");
-define('WPR_CONFIG', "$root/satis.json");
-define('WPR_EXPIRE', 365 * 24 * 60 * 60);
-define('WPR_SOURCE', "$root/artifacts");
-define('WPR_TARGET', "$root/public");
-
 if (file_exists("$root/config.php")) {
     require_once "$root/config.php";
 }
 
+$defaults = [
+    'WPR_LOG' => 'php://stdout',
+    'WPR_CACHE' => "$root/cache",
+    'WPR_CONFIG' => "$root/satis.json",
+    'WPR_EXPIRE' => 365 * 24 * 60 * 60,
+    'WPR_SOURCE' => "$root/artifacts",
+    'WPR_TARGET' => "$root/public",
+];
+
+foreach ($defaults as $key => $value) {
+    defined($key) || define($key, $value);
+}
+
 unset($root);
+unset($defaults);
+unset($key);
+unset($value);
 
 (new \WPRepo\Application())->run();
